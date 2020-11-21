bf7846ccccc507d35a8309a5b38a00070c6fffb1
山下　友輔 <>
2020-11-21 13:24:29 +0900

    calcurateの仮実装

--- code/product.py
+++ code/product.py
@@ -1,2 +1,5 @@
   #!/usr/bin/env python
   # -*- coding: utf-8 -*-
+
+def calculate_defference(control,actual):
+  return 2
--- code/test.py
+++ code/test.py
@@ -5,10 +5,19 @@ import pytest
 
 import product
 
+def test_正の数同士の引き算ができること_1():
+    input_control = 5
+    input_result = 3
+    expected = 2
+    assert product.calculate_defference(input_control,input_result) == expected
 
-
-
-
+"""
+input-actualの
+差
+絶対値を返す
+    difference = 0を期待する場合、0を返すこと
+    difference = 1を期待する場合、1を返すこと
+"""
 
 
 if __name__ == "__main__":
