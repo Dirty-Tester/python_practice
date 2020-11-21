87c44731e7ba932491b22619de41a9d943799f05
山下　友輔 <>
2020-11-21 13:51:17 +0900

    calculate_defferenceの実装を完了

--- code/test.py
+++ code/test.py
@@ -5,11 +5,19 @@ import pytest
 
 import product
 
+
+## 引き算
 def test_正の数同士の引き算ができること_1():
     assert product.calculate_defference(5,3) == 2
 
 def test_正の数同士の引き算ができること_2():
     assert product.calculate_defference(5,5) == 0
+
+def test_正の数同士の結果がマイナスになる引き算ができること():
+    assert product.calculate_defference(5,7) == -2
+
+def test_負の数と正の数の結果がマイナスになる引き算ができること():
+    assert product.calculate_defference(-5,7) == -12
 
 
 """
