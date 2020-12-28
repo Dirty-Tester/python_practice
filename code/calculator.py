eb3b786aa2f8dab936817d106342c4766ff34dff
tohru-ymgc <jimmy33hiza@yahoo.co.jp>
2020-12-28 13:41:16 +0900

    平均値算出を追加

--- code/calculator.py
+++ code/calculator.py
@@ -7,4 +7,7 @@ def calculate_difference(num_control,num_actual):
   num_of_difference = abs(num_control-num_actual)
   return num_of_difference
 
+def calculate_average(data):
+    result_average = sum(data) / len(data)
+    return result_average
 
--- code/test_calculator.py
+++ code/test_calculator.py
@@ -14,7 +14,6 @@ import calculator
 
 ## 期待値と結果の差を計算する。
 class Test_期待値と結果の差を計算する(object):
-
     def test_正の数同士の差が計算できること_1(self):
         assert calculator.calculate_difference(5,3) == 2
 
@@ -27,6 +26,9 @@ class Test_期待値と結果の差を計算する(object):
     def test_負の数と正の数の結果がマイナスだったとき差が計算できること(self):
         assert calculator.calculate_difference(-5,7) == 12
 
+    def test_格納されているデータの平均値が出せること(self):
+        assert calculator.calculate_average([1,2,3,4,5,6,7,8,9,10]) == 5.5
+
 # 他のモジュールとの兼ね合いがあるから一旦後回し(テストデータを入れたら一応できるかも)
     ## calculate_resultとactual_resultの行数が同じであること
     ## 計算結果をpandasのdataframeに格納する
@@ -36,22 +38,32 @@ class Test_期待値と結果の差を計算する(object):
 
 #　最終計算
 ## calcilate_resultの列ごとの平均値を算出し、列ごとの変数に代入する-①
-    ## 平均値が出せること
+    ## 平均値が出せる
+class test_平均値の計算(object):
+    def test_格納されているデータの平均値が出せること(self):
+        assert calculator.calculate_average([1,2,3,4,5,6,7,8,9,10]) == 5.5
+        ## dataframeの列はそのあとでやる
     ## 複数の列ごとの平均値が出せること
-    ## 複数の変数に平均値が代入出来ること
+        ## 格納されている配列(変数)に平均値が出せること
+        ## dataframeの型でやるやつはそのあとでやる
+
         ## <columm>_aerage3つできるはず
 
 ## control_dataの処理対象となった列の最大値を取得し、変数に代入する-②
     ## 列の最大値を取得できること
-    ## 複数の列ごとの平均値が出せること
+        ## 格納されているデータの最大値を取得できること
+        ## 格納されている配列(変数ごと)に最大値を取得できること
+        ## dataframeの型でやるやつはそのあとでやる
     ## 0の場合、0の除算となる
 
 ## ①/②*100を算出し、変数に代入する-③誤差率
     ## ①/②*100の計算が出来ること
     ## 変数に代入できること
+
 ## ①と②と③をテキストファイルに出力する
     ## columm_average,1,2,3
     ## max_data,1,2,3,
     ## error_rate,③
     ## uncaliculated_columm=XXX
+
 ## 処理しやすい形で出す
