846dab0a0cb9084150c9bc9ffc09bc9866866db7
山下　友輔 <38856833+yusuke-yamashita@users.noreply.github.com>
2020-12-28 13:50:30 +0900

    store_dataの作成

--- code/store_data.py
+++ code/store_data.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import os
 
 def open_dat_file(filepath):
     with open(filepath) as data:
       read_data = pd.read_table(data)
       return read_data
+
+def get_filepath(path):
+    for files in os.walk(path):
+        return files
+
+if __name__ == "__main__":
+    get_filepath("code")
--- code/test_store_data.py
+++ code/test_store_data.py
@@ -10,3 +10,6 @@ import store_data as st
 class Test_store_data(object):
     def test_ファイルパスを渡してdataframeを開くこと(self):
         assert type(st.open_dat_file("data/actual_data.dat")) == pd.core.frame.DataFrame
+
+    def test_ファイルパスを渡してファイル形式ののファイルパスを返すこと(self):
+        assert st.get_filepath("data") == ('data',[], ['control_data.dat', 'invalid_data.dat', 'actual_data.dat', 'LAG_C.dat'])
